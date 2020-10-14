25b8d38f3cb91b392f5a5c4cbee4f63ea1b85c97
Evgeniy A. Vasilenko <48185066+TzeenchH@users.noreply.github.com>
2020-10-14 23:13:35 +0300

    Update TCPServer.py

--- Lab3/TCPServer.py
+++ Lab3/TCPServer.py
@@ -1,4 +1,5 @@
 import socket
+import re
 import random
 
 more = b"more"
@@ -9,19 +10,20 @@ numb = random.randint(0, 100)
 print(numb)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind(("localhost", 5050))
+    sock.bind(("127.0.0.1", 5050))
     sock.listen(10)
     conn, addr = sock.accept()
     with conn:
         while True:
             conn.settimeout(60)
             data = conn.recv(8)
-            udata = int(data.decode("utf-8"))
-            print(udata)
-            if udata < numb:
-                print('mmm')
+            udata = data.decode("utf-8")
+            tmp = re.search(r'\d+' , udata)
+            pnb = tmp.group()
+            pendnum = int(pnb)
+            if pendnum < numb:
                 conn.sendall(more)
-            elif udata > numb:
+            elif pendnum > numb:
                 conn.send(less)
             else:
                 conn.send(correct)
