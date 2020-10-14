65d526c1a89095dd7b5282ae3d23bfafeea3828b
Evgeniy A. Vasilenko <48185066+TzeenchH@users.noreply.github.com>
2020-10-14 23:04:06 +0300

    Update TCPServer.py

--- Lab3/TCPServer.py
+++ Lab3/TCPServer.py
@@ -1,5 +1,4 @@
 import socket
-import re
 import random
 
 more = b"more"
@@ -9,27 +8,21 @@ numb = random.randint(0, 100)
 
 print(numb)
 
-sock = socket.socket()
-sock.bind( ("127.0.0.1", 5050) )
-sock.listen(10)
-try:
-    while True:
-        conn, addr = sock.accept()
-        conn.settimeout(60)
-        data = conn.recv(8)
-        udata = data.decode("utf-8")
-        tmp = re.search(r'\d+' , udata)
-        pnb = tmp.group()
-        print(type(pnb))
-        pendnum = int(pnb)
-        if pendnum > numb:
-            conn.send(more)
-        elif pendnum < numb:
-            conn.send(less)
-        else:
-            conn.send(correct)
-            conn.close()
-
-finally:
-    print("closed")
-    sock.close()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind(("localhost", 5050))
+    sock.listen(10)
+    conn, addr = sock.accept()
+    with conn:
+        while True:
+            conn.settimeout(60)
+            data = conn.recv(8)
+            udata = int(data.decode("utf-8"))
+            print(udata)
+            if udata < numb:
+                print('mmm')
+                conn.sendall(more)
+            elif udata > numb:
+                conn.send(less)
+            else:
+                conn.send(correct)
+                conn.close()
