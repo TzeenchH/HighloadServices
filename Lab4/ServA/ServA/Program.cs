2718832da11dc4841310b83a7deb0be1c89d0077
VASILENKO EVGENII <vea@akmetron.ru>
2020-10-22 01:17:35 +0300

    WIP: add graphQL handle to ServiceA

--- Lab4/ServA/ServA/Program.cs
+++ Lab4/ServA/ServA/Program.cs
@@ -4,45 +4,59 @@ using System;
 using System.Threading.Tasks;
 using GraphQL;
 using GraphQL.Types;
-using ServA.Resolvers;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ServA
 {
     class Program
     {
+
         static async Task Main(string[] args)
         {
             var schema = Schema.For(@" 
-                type Query {
-                    number: String
+                type Query{
+                    isphonevalid (number: string, code: int) {
+                        result: string
+                    }
                 }
                ");
             using var channel = GrpcChannel.ForAddress("https://localhost:5050");
             var client = new PhoneValidatorProtos.Numberer.NumbererClient(channel);
-            string number = "";
+            IReadOnlyCollection<int> codes = new int[] { 26, 42, 13, 24 };
+            int code = 11;
             var reply = await client.ValidateNumberAsync(new NumberRequest { Number = number });
             if (reply.Result)
             {
-
+                if (codes.Contains<int>(code))
+                {
+                    var res = new { Result = "valid" };
+                    schema.ExecuteAsync(_ =>
+                    {
+                        _.Query = "{isphonevalid}";
+                        _.Root = res;
+                    });
+                }
+                else
+                {
+                    var res = new { Result = "not valid" };
+                    schema.ExecuteAsync(_ =>
+                    {
+                        _.Query = "{isphonevalid}";
+                        _.Root = res;
+                    });
+                }
             }
             else
             {
-
+                var res = new { Result = "phone not found" };
+                schema.ExecuteAsync( _ =>
+                {
+                    _.Query = "{isphonevalid}";
+                    _.Root = res;
+                });
             }
         }
 
-        private Schema CreateSchema()
-        {
-            var numberField = new FieldType();
-            numberField.Name = "number";
-            numberField.ResolvedType = new StringGraphType();
-            numberField.Type = typeof(string);
-            numberField.Resolver = new NumberFieldResolver();
-            var codeField = new FieldType();
-            codeField.Name = "code";
-            codeField.ResolvedType = new StringGraphType();
-            codeField.Type = typeof(string);
-            codeField.Resolver = new CodeFieldResolver();
-        }
     }
 }
